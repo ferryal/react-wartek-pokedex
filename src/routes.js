84b14e8ac55c1bc7e517dd43a0f30757f08d3148
ferryal fajar <ferryalmfajar@gmail.com>
2021-11-10 07:03:01 +0700

    fix: change path routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import ListPokemon from './schenes/List-Pokemon';
-import PokemonDetail from './schenes/Pokemon-Detail';
+import ListPokemon from './pages/List-Pokemon';
+import PokemonDetail from './pages/Pokemon-Detail';
 
 const Routes = () => (
   <div>
